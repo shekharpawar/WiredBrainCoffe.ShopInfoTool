c0c1e02b07ed4298974f3b11ce121a55ed423e73
MSDOMAIN1\SPawar1 <shekhar.pawar@morningstar.com>
2020-06-13 12:19:10 +0530

    Add Project

---
+++ WiredBrainCoffe.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffe.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Wiredbraincoffe Shop info tool");
+        }
+    }
+}
